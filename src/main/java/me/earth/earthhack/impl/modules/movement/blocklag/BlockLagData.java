c11d749399a851ee4aa01d8bc12f2da501f24d49
nukiz <61660487+nukiz@users.noreply.github.com>
2023-03-06 21:26:28 +0200

    [1.8.7] Burrow - Update descriptions

--- src/main/java/me/earth/earthhack/impl/modules/movement/blocklag/BlockLagData.java
+++ src/main/java/me/earth/earthhack/impl/modules/movement/blocklag/BlockLagData.java
@@ -11,13 +11,13 @@ final class BlockLagData extends DefaultData<BlockLag>
                 "V-clips the specified amount down to cause a lagback." +
                         " Don't touch, 9 should be perfect.\n" +
                         "For modes Constant and Smart.");
-        register(module.timerAmount, "What the timer should be set to.");
+        register(module.timerAmount, "What the timer should be set to. In development!!!!");
         register(module.motionAmount, "Amount of motion the player should get.");
         register(module.useTimer, "Use timer when attempting lagback to" +
-                        " make it easier to accomplish");
+                        " make it easier to accomplish. In development!!!!");
         register(module.useBlink, "New revolutionary bypass for Crystalpvp.cc." +
                 " Temporarily disables sending any CPacketPlayer packets, so" +
-                " you can burrow in this time.");
+                " you can burrow in this time. In development, but functional!");
         register(module.blinkDuration, "How long Blink should be used for. For cc," +
                 " this is pretty good and stable at around 1000ms." +
                 " Not having this high enough can result in failure." +
