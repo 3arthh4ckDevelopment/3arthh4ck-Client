a98c3430cb5072b1addb432e16413e8d8a412198
nukkkiz <61660487+nukiz@users.noreply.github.com>
2023-03-17 20:48:14 +0200

    [1.8.7] Speed - SneakCheck is 100 times better

--- src/main/java/me/earth/earthhack/impl/modules/movement/speed/SpeedMode.java
+++ src/main/java/me/earth/earthhack/impl/modules/movement/speed/SpeedMode.java
@@ -14,6 +14,9 @@ public enum SpeedMode implements Globals
         @Override
         public void move(MoveEvent event, Speed module)
         {
+
+            if (module.sneakCheck.getValue())
+               if(mc.player.isSneaking() || KeyBoardUtil.isKeyDown(mc.gameSettings.keyBindSneak)) return;
             if (mc.player.isElytraFlying()) return;
             if (module.LONG_JUMP.isEnabled()) return;
             if (!module.noWaterInstant.getValue()
