ce1f612bed92827440cde376c12aa4e070d228c8
nukiz <61660487+nukiz@users.noreply.github.com>
2023-03-22 17:40:04 +0200

    [1.8.7] CevBreaker - Update some descriptions

--- src/main/java/me/earth/earthhack/impl/modules/combat/cevbreaker/CevBreakerData.java
+++ src/main/java/me/earth/earthhack/impl/modules/combat/cevbreaker/CevBreakerData.java
@@ -3,26 +3,26 @@ package me.earth.earthhack.impl.modules.combat.cevbreaker;
 import me.earth.earthhack.api.module.data.DefaultData;
 
 public class CevBreakerData extends DefaultData<CevBreaker> {
-    public CevBreakerData(CevBreaker cb){
-        super(cb);
-        register(cb.mode, "CevBreaker mode." +
+    public CevBreakerData(CevBreaker module){
+        super(module);
+        register(module.mode, "CevBreaker mode." +
                 "\n- Normal : Completes a complete cycle of placing crystal, mining," +
                 ", breaking and placing obby and repeat." +
                 "\n- Instant : Tries to instantly break obsidian and placing a crystal and repeating." +
                 " Likely doesn't work on newer servers.");
-        register(cb.range, "Select a maximum range at which CevBreaker" +
+        register(module.range, "Select a maximum range at which CevBreaker" +
                 " can place/break etc.");
-        register(cb.toggleAt, "Not implemented yet.");
-        register(cb.enemyRange, "Players within this range will be " +
+        register(module.toggleAt, "Not implemented yet.");
+        register(module.enemyRange, "Players within this range will be " +
                 "considered enemies, and attacked.");
-        register(cb.delay, "Delay between redoing a cycle after finishing the first one.");
-        register(cb.cooldown,"For servers that require you to wait a little" +
+        register(module.delay, "Delay between redoing a cycle after finishing the first one.");
+        register(module.cooldown,"For servers that require you to wait a little" +
                 " before starting to attack after switching hand. Check AutoCrystal - Cooldown.");
-        register(cb.rotate, "Whether or not to rotate.");
-        register(cb.reCheckCrystal, "Performs extra checks to see if a crystal already exists");
-        register(cb.airCheck, "Checks whether or not the block above the target is " +
+        register(module.rotate, "Whether or not to rotate.");
+        register(module.reCheckCrystal, "Performs extra checks to see if a crystal already exists");
+        register(module.airCheck, "Checks whether or not the block above the target is " +
                 "already air, and whether or not to explode a crystal");
-        register(cb.smartSneak, "Sends sneak packets depending on your situation.");
-        register(cb.bypass, "Alternate method to switching.");
+        register(module.smartSneak, "Sends sneak packets depending on your situation.");
+        register(module.bypass, "Alternate method to switching.");
     }
 }
