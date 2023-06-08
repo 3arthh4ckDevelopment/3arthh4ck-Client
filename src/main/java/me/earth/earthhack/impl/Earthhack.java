f5bd9e0004122d03356699e1c2aa44e4274a3338
nukiz <61660487+nukiz@users.noreply.github.com>
2023-06-08 20:27:20 +0300

    [1.8.9] Bump version from `1.8.8` to `1.8.9`

--- src/main/java/me/earth/earthhack/impl/Earthhack.java
+++ src/main/java/me/earth/earthhack/impl/Earthhack.java
@@ -17,7 +17,7 @@ public class Earthhack implements Globals
 {
     private static final Logger LOGGER = LogManager.getLogger("3arthh4ck");
     public static final String NAME = "3arthh4ck";
-    public static final String VERSION = "1.8.8";
+    public static final String VERSION = "1.8.9";
 
     public static void preInit()
     {
