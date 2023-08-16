17080d0c926ac89d90842e78b1e5d6a2f1e61d0a
NCPDisabler <86876065+NCPUser@users.noreply.github.com>
2023-08-16 12:19:10 +0300

    crit mode 2

--- src/main/java/me/earth/earthhack/impl/modules/combat/criticals/CriticalsData.java
+++ src/main/java/me/earth/earthhack/impl/modules/combat/criticals/CriticalsData.java
@@ -9,6 +9,7 @@ final class CriticalsData extends DefaultData<Criticals>
         super(module);
         register(module.delay, "-Packet will deal criticals silently." +
                 " \n-Bypass is like packet but works on 2b2t." +
+                " \n-NCP packet bypass like NoCheatPlus servers." +
                 " \n-Jump will automatically jump." +
                 " (Only use this on your client, not on the PingBypass)" +
                 " \n -MiniJump will jump as well but less." +
